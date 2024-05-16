bee4bc00e934bb9b81e9973910ec713a2e7e0b3b
jaydynite <jayjun3577@gmail.com>
2024-05-16 09:06:16 +0900

    Back to hello world.

--- src/App.java
+++ src/App.java
@@ -1,7 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
-        System.out.println("I'm Dr. Karel, M.D.");
-        System.out.println(x:"blah");
     }
 }
