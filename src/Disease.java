b7f71bec687f035c765e30aa085cb5132ac1a63d
jaydynite <jayjun3577@gmail.com>
2024-05-23 09:01:48 +0900

    Disease class

--- src/Disease.java
+++ src/Disease.java
@@ -1,70 +1,57 @@
 public class Disease
 {
+    private int fit;
     private String diseaseName;
-    private boolean fever;
     private double temp;
-    private boolean fatigue;
-    private boolean cough;
-    private boolean shortnessOfBreath;
-    private boolean soreThroat;
-    private boolean runnyNose;
-    private boolean lossOfTasteOrSmell;
-    private boolean headache;
-    private boolean bodyAche;
-    private boolean chills;
-    private boolean gastrointestinalSymptoms;
-    private boolean closeContact;
-    private boolean travelHistory;
-    private boolean skinRashes;
-    private boolean jointPain;
-    private boolean visionProblems;
-    private boolean swollenLymphNodes;
+    private boolean[] symptoms;
     private int age;
     private boolean gender;//true = male; false = female
 
-    public Disease(String diseaseName, 
-                   boolean fever,
+    public Disease(int fit,
+                   String diseaseName, 
                    double temp,
-                   boolean fatigue,
                    boolean cough,
-                   boolean shortnessOfBreath,
                    boolean soreThroat,
                    boolean runnyNose,
-                   boolean lossOfTasteOrSmell,
-                   boolean headache,
-                   boolean bodyAche,
                    boolean chills,
-                   boolean gastrointestinalSymptoms,
-                   boolean closeContact,
-                   boolean travelHistory,
-                   boolean skinRashes,
-                   boolean jointPain,
-                   boolean visionProblems,
-                   boolean swollenLymphNodes,
                    int age,
                    boolean gender)
     {
+        this.fit = fit;
         this.diseaseName = diseaseName;
-        this.fever = fever;
         this.temp = temp;
-        this.fatigue = fatigue;
-        this.cough = cough;
-        this.shortnessOfBreath = shortnessOfBreath;
-        this.soreThroat = soreThroat;
-        this.runnyNose = runnyNose;
-        this.lossOfTasteOrSmell = lossOfTasteOrSmell;
-        this.headache = headache;
-        this.bodyAche = bodyAche;
-        this.chills = chills;
-        this.gastrointestinalSymptoms = gastrointestinalSymptoms;
-        this.closeContact = closeContact;
-        this.travelHistory = travelHistory;
-        this.skinRashes = skinRashes;
-        this.jointPain = jointPain;
-        this.visionProblems = visionProblems;
-        this.swollenLymphNodes = swollenLymphNodes;
+        this.symptoms = new boolean[] {cough, soreThroat, runnyNose, chills};
         this.age = age;
         this.gender = gender;
     }
+
+    public int getFit()
+    {
+        return fit;
+    }
+
+    public String getDiseaseName()
+    {
+        return diseaseName;
+    }
     
+    public double getTemp()
+    {
+        return temp;
+    }
+
+    public boolean[] getSymptoms()
+    {
+        return symptoms;
+    }
+    
+    public int getAge()
+    {
+        return age;
+    }
+
+    public boolean isGender()
+    {
+        return gender;
+    }
 }
